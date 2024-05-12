27b5625b4959a44460a201501c2a67ec6b61a383
AdnaNation <azizulislamadnan@gmail.com>
2024-05-13 00:39:49 +0600

    get booked services status

--- src/Pages/BookedServices.jsx
+++ src/Pages/BookedServices.jsx
@@ -16,9 +16,51 @@ const BookedServices = () => {
             You have not booked any service yet
           </p>
         )}
-        {myBookedServices.map((service) => (
-          <div key={service._id}>{service.serviceName}</div>
-        ))}
+
+        <p className="text-center text-3xl md:text-4xl font-platypi underline mt-4">
+          Booking Status
+        </p>
+
+        <div className="space-y-2">
+          {myBookedServices.map((service) => (
+            <div
+              key={service._id}
+              className="max-w-2xl p-8 sm:flex sm:space-x-6 dark:bg-gray-50 dark:text-gray-800 border rounded-md shadow-xl mx-auto"
+            >
+              <div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
+                <img
+                  src={service.serviceImg}
+                  alt=""
+                  className="object-cover object-center w-full h-full rounded dark:bg-gray-500"
+                />
+              </div>
+              <div className="flex flex-col space-y-4 flex-grow">
+                <div>
+                  <h2 className="text-2xl font-semibold">
+                    {service.serviceName}
+                  </h2>
+                  <span className="text-sm dark:text-gray-600">
+                    Service ID: {service.serviceID}
+                  </span>
+                </div>
+                <div className="flex flex-col">
+                  <span className="dark:text-gray-600">
+                    Service Fee: ${service.price}
+                  </span>
+
+                  <span className="dark:text-gray-600">
+                    Booking for: {service.serviceDate}
+                  </span>
+                </div>
+                <div className="flex justify-end">
+                  <button className=" btn-block bg-red-500 max-w-24 rounded-lg text-white">
+                    {service.serviceStatus}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
