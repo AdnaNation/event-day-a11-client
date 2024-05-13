7d521365d53cade7b547d269d30a73ffcaa3ef99
AdnaNation <azizulislamadnan@gmail.com>
2024-05-13 19:50:54 +0600

    google login added in register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import { updateProfile } from "firebase/auth";
+import { GoogleAuthProvider, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,8 +7,9 @@ import useAuth from "../hooks/useAuth";
 
 const Register = () => {
   const navigate = useNavigate();
-  const { createUser, logOut } = useAuth();
+  const { createUser, logOut, googleSignIn, setUser } = useAuth();
   const [registerError, setRegisterError] = useState("");
+  const googleProvider = new GoogleAuthProvider();
   const handleRegister = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -50,6 +51,25 @@ const Register = () => {
       .catch((error) => {
         console.log(error);
         setRegisterError("The email you provided is already in-use");
+      });
+  };
+
+  const handleGoogleSingIn = () => {
+    googleSignIn(googleProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser.photoURL);
+        setUser(loggedInUser);
+        Swal.fire({
+          title: "Congrats!",
+          text: "You have successfully logged in",
+          icon: "success",
+        });
+        // navigate after login
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        console.log(error.message);
       });
   };
   return (
@@ -134,6 +154,24 @@ const Register = () => {
             className="btn btn-block btn-primary text-white shadow-xl my-4"
           />
         </form>
+        <p className="text-center">OR</p>
+
+        <div className="flex justify-center space-x-4">
+          <button
+            onClick={handleGoogleSingIn}
+            aria-label="Log in with Google"
+            className="p-3 rounded-lg btn w-full bg-white shadow-xl my-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              className="w-5 h-5 fill-current"
+            >
+              <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+            </svg>
+            Log in with Google
+          </button>
+        </div>
         <p className="text-center mt-3">
           Already have a account? Please{" "}
           <Link to="/login" className="underline text-blue-800">
